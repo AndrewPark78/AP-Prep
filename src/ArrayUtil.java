a6c1cc1556492c610607767ae8a69dfac8f9ce7c
NoneOfUrBuisnes <league@WorthyMini-6.attlocal.net>
2017-02-03 18:33:42 -0800

    ap prep creation & array reversal

---
+++ src/ArrayUtil.java
@@ -0,0 +1,52 @@
+public class ArrayUtil {
+	
+	/** Reverse elements of array arr
+	*   Precondition: arr.length > 0.
+	*   Postcondition: The elements of arr have been reversed
+	*   @param arr the array to manipulate
+	*/
+	public static void reverseArray(int[] arr){
+		int loc = arr.length-1;
+		int[] vzl = new int[5];
+		System.arraycopy(arr, 0, vzl, 0, 1);
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = vzl[loc];
+			loc--;
+		}
+	for(int i: arr){
+		System.out.println(i);
+	}
+	}
+}
+
+class Matrix{
+	private int[][] mat;
+	
+	public Matrix(int[][] m){
+		mat = m;
+	}
+	
+	/** Revereses the elements in each row of mat.
+	*   Postcondition: The elements in each row have been reversed
+	*/
+	public void reverseAllRows(){
+		/* code goes here */
+	}
+	
+	/** Reverses the elements of mat.
+	*   Postcondition:
+	*   - The final elements of mat, when read in row-major order, 
+	*     are the same as the original elements of mat when read
+	*     from the bottom corner, right to left, going upward.
+	*   - mat[0][0] contains what was originally the last element.
+	*   - mat[mat.length - 1][mat[0].length -1] contains what was
+	*     originally the first element.
+	*/
+	public void reverseMatrix(){
+		/* code goes here */
+	}
+	
+	public int[][] getIntArray(){
+		return mat;
+	}
+}
